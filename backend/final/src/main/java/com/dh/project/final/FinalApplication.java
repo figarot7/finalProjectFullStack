47d5c75b2a585f0a81886672afd46ef729b041ee
figarot7 <figarot7@gmail.com>
2017-06-17 00:18:16 -0400

    Configurando backend

---
+++ backend/final/src/main/java/com/dh/project/final/FinalApplication.java
@@ -0,0 +1,12 @@
+package com.dh.project.final;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinalApplication.class, args);
+	}
+}
