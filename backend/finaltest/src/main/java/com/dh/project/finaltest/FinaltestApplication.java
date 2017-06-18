99d404545c537fc1627f2ccdd045fe2007959a53
figarot7 <figarot7@gmail.com>
2017-06-17 21:48:55 -0400

    Hello world back-end

--- backend/finaltest/src/main/java/com/dh/project/finaltest/FinaltestApplication.java
+++ backend/finaltest/src/main/java/com/dh/project/finaltest/FinaltestApplication.java
@@ -2,9 +2,17 @@ package com.dh.project.finaltest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class FinaltestApplication {
+
+	@RequestMapping("/")
+	String home(){
+		return "Welcome to final project";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(FinaltestApplication.class, args);
