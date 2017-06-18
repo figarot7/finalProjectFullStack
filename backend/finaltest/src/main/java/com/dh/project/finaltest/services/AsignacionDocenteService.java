e8e352264058c829d6cf247b7b81626334586e6c
figarot7 <figarot7@gmail.com>
2017-06-17 23:08:28 -0400

    Estructurando el codigo

---
+++ backend/finaltest/src/main/java/com/dh/project/finaltest/services/AsignacionDocenteService.java
@@ -0,0 +1,10 @@
+package com.dh.project.finaltest.services;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by skypper on 17-06-17.
+ */
+@Service
+public class AsignacionDocenteService {
+}
---
+++ backend/finaltest/src/main/java/com/dh/project/finaltest/web/MateriaController.java
@@ -0,0 +1,12 @@
+package com.dh.project.finaltest.web;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by skypper on 17-06-17.
+ */
+@RestController
+@RequestMapping("/materias")
+public class MateriaController {
+}
